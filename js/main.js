77d061e3d22b74b04f2020ab80c11b62f669ab48
alikarpuzoglu <talik01@gmail.com>
2018-09-06 17:00:38 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var skill = document.getElementById('skill');
+
+
+var skills = ["development","security","UX"];
+/* gimmicky?
+*/
+function changeSkills(i)
+{
+  skill.innerText=skills[i%skills.length]
+  if(i>50){
+      return;
+  }
+  setTimeout(
+
+      function(){changeSkills((i+1));}, 1000);
+}
+
+
+  
